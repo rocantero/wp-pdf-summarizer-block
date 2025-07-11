537cc4466294b46ec76c6552e4bcf3004bf4036c
Rodrigo Cantero <rodrigo@lyradesigns.com>
2025-07-11 00:31:10 -0300

    working

--- build/claude-pdf-summarizer/index.asset.php
+++ build/claude-pdf-summarizer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '76d65e7f307c81ba0772');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ee7c981e59dbd6e6644e');
